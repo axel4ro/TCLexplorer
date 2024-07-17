3a3dc50b1ce750c17d3fb1bd6046ba7173df8e11
Axel4RO <104903581+axel4ro@users.noreply.github.com>
2024-07-17 20:07:18 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,10 +1,8 @@
-// Funcția pentru toggle a secțiunilor
 function toggleSection(sectionId) {
     const section = document.getElementById(sectionId);
     section.style.display = (section.style.display === 'none' || section.style.display === '') ? 'block' : 'none';
 }
 
-// Funcția pentru căutare
 document.getElementById('search-input').addEventListener('input', function() {
     const query = this.value.toLowerCase();
     const elements = document.querySelectorAll('.content .toggle-content');
@@ -18,22 +16,4 @@ document.getElementById('search-input').addEventListener('input', function() {
             element.style.display = 'block';
         }
     });
-});
-
-// Funcția pentru toggle a temei
-const themeToggleButton = document.getElementById('theme-toggle-button');
-const themeIcon = document.getElementById('theme-icon');
-
-themeToggleButton.addEventListener('click', function() {
-    const body = document.body;
-
-    if (body.classList.contains('dark-mode')) {
-        body.classList.remove('dark-mode');
-        body.classList.add('light-mode');
-        themeIcon.src = 'sun.png';  // Imagine pentru light mode
-    } else {
-        body.classList.remove('light-mode');
-        body.classList.add('dark-mode');
-        themeIcon.src = 'moon.png';  // Imagine pentru dark mode
-    }
 });
